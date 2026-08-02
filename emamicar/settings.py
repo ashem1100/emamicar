d1f12d2eb6027c2bacdcaa3122114e6930e0524d
ashem1100 <ashkanemami2005@gmail.com>
2026-08-02 19:25:19 +0330

    fix bugs

--- emamicar/settings.py
+++ emamicar/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$-b3$bwk!c_+da!v9^#c^&8uswj-xnot5k9948s+hjsufn+)@('
+SECRET_KEY = 'D=5ovUcbT68kn_QC-lx-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
